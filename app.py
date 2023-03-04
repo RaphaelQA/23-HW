38875287ade47e60dff346204b1a4fb4e87bef84
yraf2 <y.raf2018@yandex.ru>
2023-03-04 14:27:01 +0300

    23 HW

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify, Blueprint
+from marshmallow import ValidationError
+
+from bulider import build_query
+from models import RequestSchema
 
 app = Flask(__name__)
 
@@ -10,8 +14,22 @@ DATA_DIR = os.path.join(BASE_DIR, "data")
 
 @app.route("/perform_query")
 def perform_query():
-    # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
-    # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
-    # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
-    # вернуть пользователю сформированный результат
+    data = request.json
+    try:
+        RequestSchema().load(data)
+    except ValidationError as error:
+        return jsonify(error.messages), 400
+    first_result = build_query(
+        cmd=data['cmd1'],
+        value=data['value1'],
+        file_name=data['file_name'],
+        data=None
+    )
+    second_result = build_query(
+        cmd=data['cmd1'],
+        value=data['value1'],
+        file_name=data['file_name'],
+        data=first_result
+    )
+    return jsonify(second_result)
     return app.response_class('', content_type="text/plain")
